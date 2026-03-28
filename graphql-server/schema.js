e66132f6545660c27a6cdbe5b03d1959a2644c15
umidrza <umidrza4@gmail.com>
2026-03-28 15:35:51 +0400

    refactor server

---
+++ graphql-server/schema.js
@@ -0,0 +1,36 @@
+const typeDefs = /* GraphQL */ `
+  enum YesNo {
+    YES
+    NO
+  }
+
+  type Address {
+    street: String!
+    city: String!
+  }
+
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons(phone: YesNo): [Person!]!
+    findPerson(name: String!): Person
+  }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+    editNumber(name: String!, phone: String!): Person
+  }
+`;
+
+module.exports = typeDefs;
