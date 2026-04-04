d562748a24e8edb6dc1535ab99129a7c3ca659b7
umidrza <umidrza4@gmail.com>
2026-04-04 15:40:24 +0400

    add person fragment

--- graphql-client/src/queries.js
+++ graphql-client/src/queries.js
@@ -1,5 +1,17 @@
 import { gql } from '@apollo/client'
 
+const PERSON_DETAILS = gql`
+  fragment PersonDetails on Person {
+    id
+    name
+    phone 
+    address {
+      street 
+      city
+    }
+  }
+`
+
 export const ALL_PERSONS = gql`
   query {
     allPersons {
@@ -13,15 +25,11 @@ export const ALL_PERSONS = gql`
 export const FIND_PERSON = gql`
   query findPersonByName($nameToSearch: String!) {
     findPerson(name: $nameToSearch) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
+      ...PersonDetails
     }
   }
+
+  ${PERSON_DETAILS}
 `
 
 export const CREATE_PERSON = gql`
@@ -32,15 +40,11 @@ export const CREATE_PERSON = gql`
     $phone: String
   ) {
     addPerson(name: $name, street: $street, city: $city, phone: $phone) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
+      ...PersonDetails
     }
   }
+
+  ${PERSON_DETAILS}
 `
 
 export const EDIT_NUMBER = gql`
